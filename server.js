af9bcedb92cb7d2c68a34e8ea3b1f2b0b938a0c6
Mirsad33 <mabedinoski3387@gmail.com>
2024-05-04 16:18:26 -0400

    changes to file path

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use('/api', apiRoutes);
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
